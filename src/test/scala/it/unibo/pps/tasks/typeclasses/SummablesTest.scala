afa3143bd80b84d87a2418878854dddfc6db97ce
Enrico Marchionni <enrico.marchionni@studio.unibo.it>
2026-06-08 18:47:17 +0200

    Add part2 summables

---
+++ src/test/scala/it/unibo/pps/tasks/typeclasses/SummablesTest.scala
@@ -0,0 +1,24 @@
+package it.unibo.pps.tasks.typeclasses
+
+import org.junit.Test
+import org.junit.Assert.{assertEquals, assertFalse, assertTrue}
+
+import it.unibo.pps.u03.Sequences.Sequence.{Cons, Nil}
+import it.unibo.pps.u04.tasks.typeclasses.Ex4Summables.{sumAll, sumAllInt}
+
+class SummablesTest:
+  val si = Cons(10, Cons(20, Cons(30, Nil())))
+  val sd = Cons(10.0, Cons(20.0, Cons(30.0, Nil())))
+  val ss = Cons("10", Cons("20", Cons("30", Nil())))
+
+  @Test def testSumInt(): Unit =
+    assertEquals(60, sumAllInt(si))
+
+  @Test def testSumTypeClassInt(): Unit =
+    assertEquals(60, sumAll(si))
+
+  @Test def testSumTypeClassDouble(): Unit =
+    assertEquals(60.0, sumAll(sd), 0.05)
+
+  @Test def testSumTypeClassString(): Unit =
+    assertEquals("102030", sumAll(ss))
